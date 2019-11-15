37a74362a52549f908ec57514f90f717493b29eb
niijima <izumi_niijima@ttsystems.com>
2019-11-15 14:13:09 +0900

    不要コメントの削除

--- snippets/install-docker.sh
+++ snippets/install-docker.sh
@@ -1,25 +1,6 @@
 #!/bin/bash
 set -vxue
 
-#readonly BASE_URL="https://download.docker.com/linux/debian/dists/$(lsb_release -cs)/pool/stable/amd64/"
-#wget https://download.docker.com/linux/debian/dists/$(lsb_release -cs)/pool/stable/amd64/
-
-#sudo apt-get update
-#sudo apt-get install -y \
-#  apt-transport-https \
-#  ca-certificates \
-#  curl \
-#  gnupg2 \
-#  software-properties-common
-#curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-#sudo add-apt-repository \
-#  "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-
-# root ユーザー以外で docker を実行できる危険性については理解して利用すること
-#sudo groupadd docker
-#sudo usermod -a -G docker $(whoami)
-#sudo service docker start
-
 readonly TMP_FILE=$(mktemp)
 trap "rm -f ${TMP_FILE}" EXIT
 
